be61f4eec5e074575217a2a4ed996d82fa88d768
DWilken02 <Dylan.Wilken06@gmail.com>
2023-09-01 15:02:10 -0700

    WIP add more to JavaScript

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,22 @@ function hideResultsAndError() {
   document.getElementById("male").setAttribute("class", "hidden");
   document.getElementById("female").setAttribute("class", "hidden");
 }
+
+window.onload = function() {
+  document.querySelector("form").onsubmit = function(event) {
+    event.preventDefault();
+    hideResultsAndError();
+    const age = parseInt(document.querySelector("input#age").value);
+    const gender = parseInt(document.querySelector("input#gender").value);
+
+    if (!age || !gender) {
+      document.getElementById("error-message").removeAttribute("class");
+    } else {
+      if (age >= 21 && gender >= male) {
+        document.getElementById("male"). removeAttribute("class");
+      } else if (age >= 21 || gender >= female) {
+        document.getElementById("female").removeAttribute("class");
+      }
+    }
+  }
+}
